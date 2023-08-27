05fafa1f9d26a92e9af5357989e70debf4c795e0
Vijay Jangir <6284383+vijay-jangir@users.noreply.github.com>
2023-08-27 16:14:11 +0530

    added another function and route to get beer based on the provided id, basically implemented get method based on path receieved eg: www.....com/v1/some_endpoint/123

--- src/main/java/com/vijayjangir/spring6restmvc/controllers/BeerController.java
+++ src/main/java/com/vijayjangir/spring6restmvc/controllers/BeerController.java
@@ -8,20 +8,25 @@ import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @AllArgsConstructor
 @RestController
+@RequestMapping("/api/v1/beer")
 public class BeerController {
     private final BeerService beerService;
 
-    @RequestMapping("api/v1/beer")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Beer> listBeer(){
         return beerService.listBeers();
     }
-    public Beer getBeerById(UUID id) {
+
+    @RequestMapping(value= "{beerId}", method=RequestMethod.GET)
+    public Beer getBeerById(@PathVariable("beerId") UUID id) {
         log.debug("getBeerId - In Controller. Id: " + id.toString());
         return beerService.getBeerId(id);
     }
